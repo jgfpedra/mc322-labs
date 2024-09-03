9db359c198a345be1847a808c9f00290b8a58894
Gabriel Silva <gabrielhenriquesilva002@gmail.com>
2024-09-02 21:11:46 -0300

    nomes adicionados

--- lab2/src/lab2/classes/Main.java
+++ lab2/src/lab2/classes/Main.java
@@ -1,3 +1,11 @@
+/*
+ * Nome: Gabriel Henrique Pereira Lopes da Silva
+ * RA: 200342
+ * Nome: Joao Guilherme Ferreira Pedra
+ * RA: 248349
+ * 
+ */
+
 public class Main {
     public static void main(String[] args) throws Exception {
         //Aqui você deve realizar a simulação do funcionamento do sistema.
